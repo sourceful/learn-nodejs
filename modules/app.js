951994df9576058ccd7ad8eaff3585c6d22c82bc
bevacqua <nicolasbevacqua@gmail.com>
2013-07-12 04:05:33 -0300

    updated examples

--- modules/app.js
+++ modules/app.js
@@ -3,4 +3,4 @@
 var math = require('./math.js');
 var result = math.sum(1, 2);
 
-console.log(result);
+console.log('I can\'t believe the result is:', result, '!');
---
+++ modules/several.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var app1 = require('./app.js');
+var app2 = require('./app.js');
+var app3 = require('./app.js');
